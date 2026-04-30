de1b80ca8d5755f227eaf6b2612ab26d4db07c82
jyunsu05 <jyunsu05@gmail.com>
2026-04-30 12:09:50 +0900

    수정사항
    1. 에너미들마다 채력 각각 설정해주기
    2. 아이템들 일정 범위 이상가면 삭제하기

--- Assets/1.YunSeo/Scripts/Item.cs
+++ Assets/1.YunSeo/Scripts/Item.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class Item : MonoBehaviour
@@ -9,5 +10,14 @@ public class Item : MonoBehaviour
     {
         rigid = GetComponent<Rigidbody2D>();
         rigid.linearVelocity = Vector2.down * 0.5f;
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector2.down * 0.1f * Time.deltaTime);
+        if (transform.position.y <= -7f)
+        {
+            Destroy(gameObject);
+        }
     }
 }
